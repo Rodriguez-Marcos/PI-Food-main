f9e74e2390e715f0aaa4d380e57473d7f8b5ef34
Marcos Rodriguez <rodriguezzmarcos99@gmail.com>
2021-09-23 12:36:33 -0300

    agregados cors

--- client/src/constants.js
+++ client/src/constants.js
@@ -1,3 +1,2 @@
-export const HOST_API = 'http://localhost:3001';
-export const ROUTE_RECIPES = HOST_API + '/recipes'; // ------> esto devuelve todas las recetas de la base de datos
-export const ROUTE_TYPES = HOST_API + '/types';
+export const ROUTE_RECIPES = '/recipes'; // ------> esto devuelve todas las recetas de la base de datos
+export const ROUTE_TYPES ='/types';
